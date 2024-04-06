21accf1811ac80ef549561a1e08bf57ba619f1c3
Kerimberdi <amanokerim@gmail.com>
2024-04-06 13:41:56 +0500

    V1.0.5+24: FIX order statuses

--- lib/presentation/utils/order_statuses.dart
+++ lib/presentation/utils/order_statuses.dart
@@ -2,13 +2,14 @@ import '../../app/generated/l10n.dart';
 import '../../domain/entities/order/order_status.dart';
 
 final Map<String?, OrderStatus> orderStatuses = {
-  'ORDERPLACED': OrderStatus(S.current.orderPlaced, 'orderplaced.png'),
-  'ACCEPTED': OrderStatus(S.current.accepted, 'accepted.png'),
-  'DECLINED': OrderStatus(S.current.declined, 'declined.png'),
-  'READYTOSHIP': OrderStatus(S.current.readyToShip, 'readytoship.png'),
-  'SHIPPING': OrderStatus(S.current.shipping, 'shipping.png'),
-  'READYTODELIVER': OrderStatus(S.current.readyToDeliver, 'readytodeliver.png'),
-  'DELIVERING': OrderStatus(S.current.delivering, 'delivering.png'),
-  'DELIVERED': OrderStatus(S.current.delivered, 'delivered.png'),
-  'RETURNED': OrderStatus(S.current.returned, 'returned.png'),
+  'SARGYT EDILDI': OrderStatus(S.current.orderPlaced, 'orderplaced.png'),
+  'KABUL EDILDI': OrderStatus(S.current.accepted, 'accepted.png'),
+  'ÝÜZ ÖWRÜLDI': OrderStatus(S.current.declined, 'declined.png'),
+  'IBERMÄGE TAÝÝAR': OrderStatus(S.current.readyToShip, 'readytoship.png'),
+  'UGRADYLDY': OrderStatus(S.current.shipping, 'shipping.png'),
+  'GOWŞURYLMAGA TAÝÝAR':
+      OrderStatus(S.current.readyToDeliver, 'readytodeliver.png'),
+  'GOWŞURYLÝAR': OrderStatus(S.current.delivering, 'delivering.png'),
+  'GOWŞURYLDY': OrderStatus(S.current.delivered, 'delivered.png'),
+  'YZYNA GAÝTARYLDY': OrderStatus(S.current.returned, 'returned.png'),
 };
